b789e11fa3ed071eb2b5e321d3a0cec445db30b3
vakabunga <shesterov@gmail.com>
2024-01-13 23:30:15 +0300

    n-th try to install stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,10 @@
 const { propertyOrdering, selectorOrdering } = require('stylelint-semantic-groups');
 
 module.exports = {
-    extends: ['@shopify/stylelint-plugin'],
+    extends: ['@shopify/stylelint-plugin/prettier'],
     plugins: ['stylelint-order'],
     rules: {
-        'order/order': selectorOrdering,
+        'order/order': selectorOrdering, // to fine-tune configuration use selectorOrderFactory
         'order/properties-order': propertyOrdering,
         'selector-class-pattern': /[a-z_\-]+/
     },
